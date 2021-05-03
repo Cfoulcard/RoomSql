10cefbd805500700a9e4b66bea537c4342cc5472
Taterchip <cfoulcard@gmail.com>
2021-05-03 18:59:11 -0500

    Onclick listeners for edit modes

--- app/src/main/java/com/example/roomsql/NoteActivity.kt
+++ app/src/main/java/com/example/roomsql/NoteActivity.kt
@@ -6,6 +6,8 @@ import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
 import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.roomsql.models.Note
@@ -16,18 +18,26 @@ import com.example.roomsql.models.Note
 class NoteActivity : AppCompatActivity(),
     View.OnTouchListener,
     GestureDetector.OnGestureListener,
-    GestureDetector.OnDoubleTapListener
+    GestureDetector.OnDoubleTapListener,
+    View.OnClickListener
 {
 
     // UI components
     private var mLinedEditText: LinedEditText? = null
     private var mEditTitle: EditText? = null
     private var mViewTitle: TextView? = null
+    private var mCheckContainer: RelativeLayout? = null
+    private var mBackArrowContainer: RelativeLayout? = null
+    private var mCheck: ImageButton? = null
+    private var mBackArrow: ImageButton? = null
 
     // Variables
     private var mIsNewNote = false
     private var mNoteInitial: Note? = null
     private var mGestureDetector: GestureDetector? = null
+    private val EDIT_MODE_ENABLED = 1
+    private val EDIT_MODE_DISABLED = 0
+    private var mMode = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,12 +46,17 @@ class NoteActivity : AppCompatActivity(),
         mLinedEditText = findViewById(R.id.note_text)
         mEditTitle = findViewById(R.id.note_edit_title)
         mViewTitle = findViewById(R.id.note_text_title)
+        mCheck = findViewById(R.id.toolbar_check)
+        mBackArrow = findViewById(R.id.toolbar_back_arrow)
+        mCheckContainer = findViewById(R.id.check_container)
+        mBackArrowContainer = findViewById(R.id.back_arrow_container)
 
         setListeners()
 
         if (incomingIntent) {
             // this is a new note (EDIT MODE)
             setNewNoteProperties()
+            enableEditMode()
         } else {
             // this is note a new note (VIEW MODE)
             setNoteProperties()
@@ -52,18 +67,40 @@ class NoteActivity : AppCompatActivity(),
     private fun setListeners() {
         mLinedEditText?.setOnTouchListener(this)
         mGestureDetector = GestureDetector(this, this)
+        mCheck?.setOnClickListener(this)
+        mViewTitle?.setOnClickListener(this)
+
     }
 
     private val incomingIntent: Boolean
         get() {
             if (intent.hasExtra("selected note")) {
                 mNoteInitial = intent.getParcelableExtra("selected note")
+
+                mMode = EDIT_MODE_ENABLED
                 mIsNewNote = false
                 return false
             }
+            mMode = EDIT_MODE_ENABLED
             mIsNewNote = true
             return true
         }
+
+    private fun enableEditMode() {
+        mBackArrowContainer!!.visibility = View.GONE
+        mCheckContainer!!.visibility = View.VISIBLE
+        mViewTitle!!.visibility = View.GONE
+        mEditTitle!!.visibility = View.VISIBLE
+        mMode = EDIT_MODE_ENABLED
+    }
+
+    private fun disableEditMode() {
+        mBackArrowContainer!!.visibility = View.VISIBLE
+        mCheckContainer!!.visibility = View.GONE
+        mViewTitle!!.visibility = View.VISIBLE
+        mEditTitle!!.visibility = View.GONE
+        mMode = EDIT_MODE_DISABLED
+    }
 
     private fun setNewNoteProperties() {
         mViewTitle!!.text = "Note Title"
@@ -127,6 +164,8 @@ class NoteActivity : AppCompatActivity(),
 
     override fun onDoubleTap(e: MotionEvent?): Boolean {
         Log.d("onDoubleTap", "double tapped!")
+        enableEditMode()
+
         return false
     }
 
@@ -138,6 +177,27 @@ class NoteActivity : AppCompatActivity(),
 
     companion object {
         private const val TAG = "NoteActivity"
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.toolbar_check -> {
+                disableEditMode()
+            }
+            R.id.note_text_title -> {
+                enableEditMode()
+                mEditTitle!!.requestFocus()
+                mEditTitle!!.setSelection(mEditTitle!!.length())
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        if (mMode == EDIT_MODE_ENABLED) {
+            onClick(mCheck)
+        } else {
+            super.onBackPressed()
+        }
     }
 
 }
